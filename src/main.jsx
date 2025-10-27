c37ac42061537c4d1da90f692bd81dd461b2e00e
주현정 <61533589+joooo24@users.noreply.github.com>
2025-10-28 08:52:24 +0900

    chore: Font Awesome 설치 및 이슈 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { config } from "@fortawesome/fontawesome-svg-core"
+import "@fortawesome/fontawesome-svg-core/styles.css"
 import './index.css'
 import App from './App.jsx'
+
+// Font Awesome CSS 적용 이슈 해결
+config.autoAddCss = false
 
 createRoot(document.getElementById('root')).render(
     // <StrictMode>

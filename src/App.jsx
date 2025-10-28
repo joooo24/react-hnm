396ef2a185b5e69cca0fa49f415741bd27ec635e
주현정 <61533589+joooo24@users.noreply.github.com>
2025-10-28 09:03:41 +0900

    add: 페이지, 컴포넌트 추가 및 라우터 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Routes, Route } from 'react-router-dom'
+import Nav from './components/Nav'
+import ProductAll from './pages/ProductAll'
+import ProductDetail from './pages/ProductDetail'
+import Login from './pages/Login'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<ProductAll />} />
+        <Route
+          path="/product/:id"
+          element={<ProductDetail />}
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
   )
 }
 

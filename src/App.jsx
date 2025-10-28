df583879ee96d11d756abbeb616ccb96956890b3
claire <61533589+joooo24@users.noreply.github.com>
2025-10-28 23:46:31 +0900

    chore: reset 추가 및 코드 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-
-import { Routes, Route } from 'react-router-dom'
-import Nav from './components/Nav'
-import ProductAll from './pages/ProductAll'
-import ProductDetail from './pages/ProductDetail'
-import Login from './pages/Login'
-import PrivateRoute from './components/PrivateRoute'
-import './App.scss'
+import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import ProductAll from "./pages/ProductAll";
+import ProductDetail from "./pages/ProductDetail";
+import Login from "./pages/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import "./App.scss";
 
 function App() {
-  return (
-    <div className='app-wrap'>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<ProductAll />} />
-        <Route
-          path="/product/:id"
-          element={
-            <PrivateRoute>
-              <ProductDetail />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </div>
-  )
+    return (
+        <div className="app-wrap">
+            <Nav />
+            <Routes>
+                <Route path="/" element={<ProductAll />} />
+                <Route
+                    path="/product/:id"
+                    element={
+                        <PrivateRoute>
+                            <ProductDetail />
+                        </PrivateRoute>
+                    }
+                />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;

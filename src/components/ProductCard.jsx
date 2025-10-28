88fb16822d5e3f8a8e26cd862e944ff4070a8472
claire <61533589+joooo24@users.noreply.github.com>
2025-10-29 00:05:34 +0900

    add: 제품 컨테이너, 제품 카드 작업

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./ProductCard.scss";
+
+const ProductCard = ({ item }) => {
+    return (
+        <div className="product-card">
+            <div className="img-wrap">
+                <img src={item?.img} alt="thumbnail" />
+            </div>
+            <div className="info">
+                <h3 className="name">{item?.title}</h3>
+                <p className="price">₩{item?.price}</p>
+            </div>
+			{(item?.choice || item?.new || (item?.tags.length > 0)) && (
+                <ul className="tag-wrap">
+					{item?.choice ? <li className="choice">MD추천</li> : null}
+					{item?.new ? <li className="new">신제품</li> : null}
+					{(item?.tags || []).map((tag, index) => (
+                        <li key={index}>{tag}</li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+};
+
+export default ProductCard;
--- src/pages/ProductAll.jsx
+++ src/pages/ProductAll.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+import { Col, Container, Row } from "react-bootstrap";
 
 const ProductAll = () => {
-    return (
-        <div></div>
-    )
-}
+    const [productList, setProductList] = useState([]);
 
-export default ProductAll
+    const getProducts = async () => {
+        const url = "http://localhost:4001/products";
+        const response = await fetch(url);
+        const data = await response.json();
+        setProductList(data);
+    };
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    return (
+        <Container className="product-card-container">
+            <Row>
+                {productList.map((item) => (
+                    <Col key={item.id} xs={6} md={4} lg={3}>
+                        <ProductCard item={item} />
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
+};
+
+export default ProductAll;

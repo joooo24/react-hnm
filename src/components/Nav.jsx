f398545b39d1372c74a959c1184a16eb116c59e7
claire <61533589+joooo24@users.noreply.github.com>
2025-10-29 00:35:00 +0900

    add: Nav 구조 수정 및 스타일 적용

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,33 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faSearch } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from "react";
+import "./Nav.scss";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
     // const [keyword, setKeyword] = useState('')
-    const categories = ['아우터', '상의', '하의']
+    const [isLogin, setLogin] = useState(false);
+    const categories = ["아우터", "상의", "하의"];
     return (
         <nav className="navbar">
-            <h2 className='logo-wrap'>
-                <img src="" alt="" />
-            </h2>
-            <div className="btn-wrap">
-                <button className="btn-login" onClick={() => console.log('로그인 클릭')}>
-                    <FontAwesomeIcon icon={faUser} />
-                    <span>로그인</span>
-                </button>
-
-                <button className="btn-logout">
-                    <FontAwesomeIcon icon={faUser} />
-                    로그아웃
-                </button>
+            <div className="top-wrap">
+                <h2 className="logo-wrap">
+                    <img
+                        src="https://image.msscdn.net/display/images/2025/10/22/d9647c20f3cb47f8af1b7d4a360b936b.png"
+                        alt=""
+                    />
+                </h2>
+                <div className="btn-wrap">
+                    {isLogin ? (
+                        <button className="btn-login" onClick={() => setLogin(!isLogin)}>
+                            <span>로그아웃</span>
+                        </button>
+                    ) : (
+                        <button className="btn-login" onClick={() => setLogin(!isLogin)}>
+                            <span>로그인</span>
+                        </button>
+                    )}
+                </div>
             </div>
             <div className="nav-container">
                 <ul className="nav-menu">
@@ -31,21 +38,15 @@ const Nav = () => {
                     ))}
                 </ul>
 
-                <form className="search-box" >
-                    <input
-                        type="text"
-                        className="search-input"
-                        placeholder="상품을 검색하세요"
-                    />
+                <form className="search-box">
+                    <input type="text" className="search-input" placeholder="상품을 검색하세요" />
                     <button type="submit" className="search-button">
                         <FontAwesomeIcon icon={faSearch} />
-                        <span style={{ marginLeft: 6 }}>검색</span>
                     </button>
                 </form>
-
             </div>
         </nav>
-    )
-}
+    );
+};
 
-export default Nav
+export default Nav;

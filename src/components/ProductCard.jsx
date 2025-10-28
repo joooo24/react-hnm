81d54348bce0d4a2a13cfb4f56744ba16a6b3044
claire <61533589+joooo24@users.noreply.github.com>
2025-10-29 00:45:59 +0900

    chore: 상품상세 navigate 설정

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./ProductCard.scss";
+import { useNavigate } from "react-router-dom";
 
 const ProductCard = ({ item }) => {
+    const navigate = useNavigate();
+    const showDetail = () => {
+        navigate(`/product/${item.id}`);
+    };
+
     return (
-        <div className="product-card">
+        <div className="product-card" onClick={showDetail}>
             <div className="img-wrap">
                 <img src={item?.img} alt="thumbnail" />
             </div>
@@ -11,11 +17,11 @@ const ProductCard = ({ item }) => {
                 <h3 className="name">{item?.title}</h3>
                 <p className="price">₩{item?.price}</p>
             </div>
-			{(item?.choice || item?.new || (item?.tags.length > 0)) && (
+            {(item?.choice || item?.new || item?.tags.length > 0) && (
                 <ul className="tag-wrap">
-					{item?.choice ? <li className="choice">MD추천</li> : null}
-					{item?.new ? <li className="new">신제품</li> : null}
-					{(item?.tags || []).map((tag, index) => (
+                    {item?.choice ? <li className="choice">MD추천</li> : null}
+                    {item?.new ? <li className="new">신제품</li> : null}
+                    {(item?.tags || []).map((tag, index) => (
                         <li key={index}>{tag}</li>
                     ))}
                 </ul>
